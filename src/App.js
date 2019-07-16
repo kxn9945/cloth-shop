d126c928a3db841bf04c6b96785a8ddd4f9c5e4d
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-15 20:41:42 -0400

    add signup authen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up';
-import { auth } from './firebase/firebase';
+import {auth, createUserProfileDocument} from './firebase/firebase';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +19,27 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      //this.setState({ currentUser: user });
+      //createUserProfileDocument(user);
+      //console.log(user);
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          //console.log(snapShot.data());
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state);
+          })
+        });
+
+      }
+      this.setState({currentUser: userAuth})
     });
   }
 
@@ -31,16 +48,14 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Header currentUser={this.state.currentUser} />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
-        </Switch>
-      </div>
-    );
+    return (<div>
+      <Header currentUser={this.state.currentUser}/>
+      <Switch>
+        <Route exact="exact" path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
+      </Switch>
+    </div>);
   }
 }
 
--- src/pages/sign-in-sign-up/sign-in-sign-up.js
+++ src/pages/sign-in-sign-up/sign-in-sign-up.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './sign-in-sign-up.scss';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 const SignInAndSignUpPage = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
