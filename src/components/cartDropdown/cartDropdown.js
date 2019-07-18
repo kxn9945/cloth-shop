bd2c28bdca850174413752855c1b935b9e213da0
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-18 10:01:20 -0400

    using reselect to improve performance

--- src/components/cartDropdown/cartDropdown.js
+++ src/components/cartDropdown/cartDropdown.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button';
+import CartItem from '../cartItem/cartItem';
+import {connect} from 'react-redux';
+import {selectCartItems} from '../../redux/cart/cartSelector';
 
 import './cartDropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
+    {
+      cartItems.map(cartItem => (<CartItem key={CartItem.id} item={cartItem} />))
+    }
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   </div>
 )
 
-export default CartDropdown;
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
