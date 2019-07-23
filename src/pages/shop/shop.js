8c084b6a3bda55b773a75e993e57e05a9b84bcfc
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-23 14:16:42 -0400

    add promise

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -21,11 +21,25 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
 
+    //SNapshot firebase
+    //Observer pattern
+    // this.unsubscribeFromSnapshot = collectionRef.onSnapShot(snapshot => {
+    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionsMap);
+    //   this.setState({ loading: false });
+    // });
+
+    //Promise
     collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
     });
+
+    //fetch
+    // fetch('https://firestore.googleapis.com/v1/projects/shop-db/databases/(default)/documents/collections')
+    // .then(response => response.json())
+    // .then(collections => console.log(collections));
   }
 
   render() {
