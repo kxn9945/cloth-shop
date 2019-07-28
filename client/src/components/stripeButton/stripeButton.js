522b2417fdd156b57cf4acb7856215f57a25d183
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-27 20:29:50 -0400

    separate app into clientside and backend side

--- client/src/components/stripeButton/stripeButton.js
+++ client/src/components/stripeButton/stripeButton.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_XRaAW26WOPf6dfS8XftZIdzH003Mr0AR1s';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    //console.log(token);
+    //alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data:{
+        amount:priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert("Payment success");
+    }).catch(err => {
+      console.log('Error: ', JSON.parse(err));
+      alert('Payment Unsuccessful. Please use the provided credit card');
+    })
   }
   return (<StripeCheckout label='Pay Now' name='Kritapas Ltd.' billingAddress="billingAddress" shippingAddress="shippingAddress" image='https://svgshare.com/i/CUz.svg' description={`Your total is $${price}`} amount={priceForStripe} panelLabel='Pay Now' token={onToken} stripeKey={publishableKey}/>)
 }
