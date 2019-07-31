ae91d8e674a52336dbf3317642675e276d9db814
Kritapas Nimtrakul <kritapas333@hotmail.com>
2019-07-31 18:14:01 -0400

    fix error

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(enforce.HTTPS({trustProtoHeader: true});
+app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 app.use(cors());
 
